4378bdb5f0ec03983af67031077b35627c7c50cf
André Ferreira <contato@andreferreira.me>
2017-06-02 21:49:19 -0300

    refactor: Add more variables to JSX

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import './App.css';
 class App extends Component {
     render() {
         var helloWorld = 'Welcome to React';
+        var user = {
+            name: 'André',
+            lastName: 'Ferreira'
+        };
+        var country = 'Brazil';
+
         return (
             <div className="App">
-                <h2>{helloWorld}</h2>
+                <h2>{helloWorld}, {user.name} {user.lastName}!</h2>
+                <h3>We are in {country}</h3>
             </div>
         );
     }
