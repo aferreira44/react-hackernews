f4ee87b3a4005e887014b47ed779e624934e1934
André Ferreira <contato@andreferreira.me>
2017-07-04 18:24:12 -0300

    refactor: Refactor to functional stateless components

--- src/App.js
+++ src/App.js
@@ -70,68 +70,56 @@ class App extends Component {
         const {author, title, list, searchTerm} = this.state
         return (
             <div className="App">
-                <Header title={title} author={author} />
-                <Search value={searchTerm} onChange={this.onSearchChange}> Search: </Search>
-                <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+                <Header title={title} author={author}/>
+                <Search value={searchTerm} onChange={this.onSearchChange}>
+                    Search:
+                </Search>
+                <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss}/>
             </div>
         );
     }
 }
 
-class Header extends Component {
-    render() {
-        const { title, author } = this.props
-        return (
-            <div>
-                <h1>{title}</h1>
-                <h3>Author: {author}</h3>
-            </div>
-        )
-    }
-}
+const Header = ({title, author}) => 
+    <div>
+        <h1>{title}</h1>
+        <h3>Author: {author}</h3>
+    </div>
 
-class Search extends Component {
-    render() {
-        const { value, onChange, children } = this.props
-        return (
-            <form>
-                {children}
-                <input type="text" value={value} onChange={onChange}/>
-            </form>
-        )
-    }
+const Button = ({
+    onClick,
+    className = '',
+    children
+}) => <button onClick={onClick} className={className} type="button">{children}</button>
 
-}
+const Search = ({value, onChange, children}) =>
+<form>
+    {children}
+    <input type="text" value={value} onChange={onChange}/>
+</form>
 
-class Table extends Component {
-    render() {
-        const {list, pattern, onDismiss} = this.props
-        return (
-            <div>
-                {list
-                    .filter(isSearched(pattern))
-                    .map(item => <div key={item.objectID}>
-                        <span>
-                            <a href={item.url}>{item.title}</a>
-                        </span>
-                        <span>
-                            {item.author}
-                        </span>
-                        <span>
-                            {item.num_comments}
-                        </span>
-                        <span>
-                            {item.points}
-                        </span>
-                        <span>
-                            <button onClick={() => onDismiss(item.objectID)} type="button">Dismiss</button>
-                        </span>
-                    </div>)
-                }
-            </div>
-        )
-    }
-
-}
+const Table = ({list, pattern, onDismiss}) =>
+    <div>
+        {list
+            .filter(isSearched(pattern))
+            .map(item => <div key={item.objectID}>
+                <span>
+                    <a href={item.url}>{item.title}</a>
+                </span>
+                <span>
+                    {item.author}
+                </span>
+                <span>
+                    {item.num_comments}
+                </span>
+                <span>
+                    {item.points}
+                </span>
+                <span>
+                    <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
+                </span>
+            </div>)
+        }
+    </div>
 
 export default App;
