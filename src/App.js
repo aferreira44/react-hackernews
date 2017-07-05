b6a6a6e84c1394ebf93bc921a413fe5c004bc88d
André Ferreira <contato@andreferreira.me>
2017-07-04 21:30:36 -0300

    feat: Fetch Hacker News API and add css classes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
 
-const list = [
-    {
-        title: 'React',
-        url: 'https://facebook.github.io/react/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0
-    }, {
-        title: 'Redux',
-        url: 'https://facebook.github.io/redux/',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1
-    }
-];
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https:/hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
 
 const title = 'React App: Hello World!'
 
@@ -36,9 +26,13 @@ class App extends Component {
         this.state = {
             title,
             author,
-            list,
-            searchTerm: ''
+            result: null,
+            searchTerm: DEFAULT_QUERY
         }
+
+        this.setSearchTopStories = this
+            .setSearchTopStories
+            .bind(this);
 
         this.onDismiss = this
             .onDismiss
@@ -47,6 +41,21 @@ class App extends Component {
         this.onSearchChange = this
             .onSearchChange
             .bind(this)
+    }
+
+    setSearchTopStories(result) {
+        this.setState({result});
+    }
+
+    fetchSearchTopStories(searchTerm) {
+        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+            .then(response => response.json())
+            .then(result => this.setSearchTopStories(result));
+    }
+
+    componentDidMount(){
+        const { searchTerm } = this.state;
+        this.fetchSearchTopStories(searchTerm);
     }
 
     onDismiss(id) {
@@ -67,24 +76,29 @@ class App extends Component {
     }
 
     render() {
-        const {author, title, list, searchTerm} = this.state
+        const {author, title, searchTerm, result} = this.state;
+
+        if (!result) { return null; }
+
         return (
-            <div className="App">
-                <Header title={title} author={author}/>
-                <Search value={searchTerm} onChange={this.onSearchChange}>
-                    Search:
-                </Search>
-                <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss}/>
+            <div className="page">
+                <div className="interactions">
+                    <Header title={title} author={author}/>
+                    <Search value={searchTerm} onChange={this.onSearchChange}>
+                        Search:
+                    </Search>
+                </div>
+                <Table list={result.hits} pattern={searchTerm} onDismiss={this.onDismiss}/>
             </div>
         );
     }
 }
 
-const Header = ({title, author}) => 
-    <div>
-        <h1>{title}</h1>
-        <h3>Author: {author}</h3>
-    </div>
+// Define Components
+const Header = ({title, author}) => <div>
+    <h1>{title}</h1>
+    <h3>Author: {author}</h3>
+</div>
 
 const Button = ({
     onClick,
@@ -92,34 +106,43 @@ const Button = ({
     children
 }) => <button onClick={onClick} className={className} type="button">{children}</button>
 
-const Search = ({value, onChange, children}) =>
-<form>
+const Search = ({value, onChange, children}) => <form>
     {children}
     <input type="text" value={value} onChange={onChange}/>
 </form>
 
-const Table = ({list, pattern, onDismiss}) =>
-    <div>
-        {list
-            .filter(isSearched(pattern))
-            .map(item => <div key={item.objectID}>
-                <span>
-                    <a href={item.url}>{item.title}</a>
-                </span>
-                <span>
-                    {item.author}
-                </span>
-                <span>
-                    {item.num_comments}
-                </span>
-                <span>
-                    {item.points}
-                </span>
-                <span>
-                    <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
-                </span>
-            </div>)
-        }
-    </div>
+const Table = ({list, pattern, onDismiss}) => <div className="table">
+    {list
+        .filter(isSearched(pattern))
+        .map(item => <div key={item.objectID} className="table-row">
+            <span style={largeColumn}>
+                <a href={item.url}>{item.title}</a>
+            </span>
+            <span style={midColumn}>
+                {item.author}
+            </span>
+            <span style={smallColumn}>
+                {item.num_comments}
+            </span>
+            <span style={smallColumn}>
+                {item.points}
+            </span>
+            <span style={smallColumn}>
+                <Button onClick={() => onDismiss(item.objectID)} className="button-inline">Dismiss</Button>
+            </span>
+        </div>)
+}
+</div>
+
+// Define style objects
+const smallColumn = {
+    width: '10%'
+}
+const midColumn = {
+    width: '30%'
+}
+const largeColumn = {
+    width: '40%'
+}
 
 export default App;
